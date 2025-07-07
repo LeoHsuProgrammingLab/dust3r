07d39ca50b7fd4f75c3edf2f248bad10dfe89ae7
Leo Hsu (Hsu, Chih-Chuan) <75620039+LeoHsuProgrammingLab@users.noreply.github.com>
2025-07-07 18:08:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ print('Found packages:', find_packages())
 setup(
     description='DUSt3R as a package',
     name='dust3r',
-    version='0.0',    
+    version='1.0',    
     packages=find_packages(),
     install_requires=[
         'torch==2.5.1',
